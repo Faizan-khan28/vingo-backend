04938cdbf6ba64a169baaab804eb03509d3714af
faizanKhan <faizankhan46849@gmail.com>
2026-02-05 19:30:14 +0530

    setup cloudinary and multer

---
+++ utils/cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+const uploadOnCloudinary = async (file) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_SECREAT_KEY,
+  });
+  try {
+    const result = await cloudinary.uploader.upload(file)
+    fs.unlinkSync(file)
+    return result.secure_url
+  } catch (error) {
+    fs.unlinkSync(file)
+    console.log(error)
+  }
+};
+
+export default uploadOnCloudinary;
