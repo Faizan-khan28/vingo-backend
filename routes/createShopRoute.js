17f9b543658f350f82ce41ad3c32ce25e211fb5a
faizanKhan <faizankhan46849@gmail.com>
2026-02-08 15:38:57 +0530

    add getmyshop route

--- routes/createShopRoute.js
+++ routes/createShopRoute.js
@@ -1,9 +1,11 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { createShop } from "../controllers/shopController.js"
+import { createShop, getmyShop } from "../controllers/shopController.js"
+import upload from "../middlewares/multer.js"
 
 const shopRouter = express.Router()
 
-shopRouter.post("create-edit",isAuth,createShop)
+shopRouter.post("create-edit",isAuth,upload.singe("image"),createShop)
+shopRouter.post("get-myshop",isAuth,upload.singe("image"),getmyShop)
 
 export default shopRouter;
