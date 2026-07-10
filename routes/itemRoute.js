9c3a62020a0dcbee3aad73283482fa4b04690ef2
faizanKhan <faizankhan46849@gmail.com>
2026-07-10 17:01:13 +0530

    add deleteitem controller

--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -1,12 +1,13 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
-import { addItem, editItem, getItem} from "../controllers/itemController.js"
+import { addItem, deleteItem, editItem, getItem} from "../controllers/itemController.js"
 
 const itemRouter = express.Router()
 
 itemRouter.post("/add-item",isAuth,upload.single("image"),addItem)
 itemRouter.post("/edit-item/:itemId",isAuth,upload.single("image"),editItem)
 itemRouter.get("/get-item/:itemId",isAuth,getItem)
+itemRouter.get("/delete-item/:itemId",isAuth,deleteItem)
 
 export default itemRouter;
